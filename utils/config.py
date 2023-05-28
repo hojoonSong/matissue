f16d09e1fa7b7127bee36991d3b1b19cabbb2d09
HOJUN <bmp.tom@yahoo.com>
2023-05-28 19:16:01 +0900

    [fix] edit config.py

--- utils/config.py
+++ utils/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    mongo_db_url: str = "mongodb+srv://shinyubin18:DDYxIIRc0DJKS1Cn@fast.tfq6irt.mongodb.net/"
-    mongo_db_name: str = "FAST"
+    mongo_db_url: str
+    mongo_db_name: str
 
     class Config:
         env_file = ".env"
