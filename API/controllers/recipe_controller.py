8afd692eb556caafef60ee5f164ecbe99be32d5e
yubin <shin.yubin18@gmail.com>
2023-05-29 18:50:43 +0900

    [Add] delete 기능 추가

--- API/controllers/recipe_controller.py
+++ API/controllers/recipe_controller.py
@@ -8,3 +8,30 @@ router = APIRouter()
 @router.get("/")
 async def yummy():
     return {"recipe": "😋"}
+
+
+@router.get("/db")
+async def fetch_db():
+    return db
+
+
+@router.post("/db")
+async def register_db(recipe: Recipe):
+    db.append(recipe)
+    return {"recipe_title": recipe.recipe_title, "full": recipe}
+
+
+@router.delete("/db/{recipe_id}")
+async def delete_db(recipe_id: UUID):
+    for recipe in db:
+        if recipe.recipe_id == recipe_id:
+            db.remove(recipe)
+            return {"msg": "success"}
+
+
+@router.patch("/db/{recipe_id}")
+async def delete_db(recipe_id: UUID):
+    for recipe in db:
+        if recipe.recipe_id == recipe_id:
+            db.remove(recipe)
+            return {"msg": "success"}
